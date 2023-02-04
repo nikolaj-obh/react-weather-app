0da7e7ea50088c84fbd7f17b51d7f3aa710adf54
Nikolaj <nikolaj.hansen93@gmail.com>
2023-02-04 16:40:26 +0100

    update

---
+++ src/api.js
@@ -0,0 +1,11 @@
+export const geoApiOptions = {
+    method: "GET",
+    headers: {
+        "X-RapidAPI-Key": "7001bd065emsha29208a2b12fa90p127287jsnd2752efa753f",// enter your rapid api key here
+        "X-RapidAPI-Host": "wft-geo-db.p.rapidapi.com",
+    },
+};
+export const GEO_API_URL = "https://wft-geo-db.p.rapidapi.com/v1/geo";
+
+export const WEATHER_API_URL = "https://api.openweathermap.org/data/2.5";
+export const WEATHER_API_KEY = "d3ab0c7d63c7ff06681ba19c7bda2359"; // enter your key from openweather API
